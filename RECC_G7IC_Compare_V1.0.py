cead81263853e0eac284a60cdcd0f35cffbcfeaa
= <stefanpauliuk@gmx.de>
2019-04-22 09:23:57 +0200

    Added area plot code for RE strategy cascade.

--- RECC_G7IC_Compare_V1.0.py
+++ RECC_G7IC_Compare_V1.0.py
@@ -181,6 +181,48 @@ FolderlistB =[
 
 
 
+
+
+
+#Sensitivity analysis folder order, by default, all strategies are off, one by one is implemented each at a time.
+#Baseline (no RE)
+#FabYieldImprovement
+#EoL_RR_Improvement
+#ChangeMaterialComposition
+#ReduceMaterialContent
+#ReUse_Materials
+#LifeTimeExtension
+#MoreIntenseUse
+
+
+Region= 'G7'
+Scope = 'G7 Vehicles'
+FolderlistV_Sens =[
+'G7_2019_4_18__22_8_8',
+'G7_2019_4_19__7_55_16',
+'G7_2019_4_19__8_0_41',
+'G7_2019_4_19__8_9_6',
+'G7_2019_4_19__8_13_58',
+'G7_2019_4_19__8_19_17',
+'G7_2019_4_19__8_22_28',
+'G7_2019_4_19__8_27_9',
+]
+
+Scope = 'G7 Buildings'
+FolderlistB_Sens =[
+'G7_2019_4_18__22_27_0',
+'G7_2019_4_19__8_47_52',
+'G7_2019_4_19__9_24_11',
+'G7_2019_4_19__9_27_35',
+'G7_2019_4_19__9_32_20',
+'G7_2019_4_19__9_35_45',
+'G7_2019_4_19__9_42_3',
+'G7_2019_4_19__9_45_20',
+]
+
+
+# Waterfall plots.
+
 NS = 3
 NC = 2
 NR = 5
@@ -226,12 +268,7 @@ ASummaryB[0:3,:] = AnnEmsB2030[:,1,:].copy()
 ASummaryB[3:6,:] = AnnEmsB2050[:,1,:].copy()
 ASummaryB[6::,:] = CumEmsB[:,1,:].copy()
         
-        
-     
-
-
-     
-# Plot            
+# Waterfall plot            
 MyColorCycle = pylab.cm.Set1(np.arange(0,1,0.1)) # select 12 colors from the 'Paired' color map.            
 
 Title = ['Passenger vehicles','residential buildings']
@@ -369,9 +406,304 @@ for m in range(0,NS): # SSP
 
 
 
+# Sensitivity plots
 
+NS = 3
+NR = 8
 
+CumEmsV_Sens     = np.zeros((NS,NR)) # SSP-Scenario x RCP scenario x RES scenario
+AnnEmsV2030_Sens = np.zeros((NS,NR)) # SSP-Scenario x RCP scenario x RES scenario
+AnnEmsV2050_Sens = np.zeros((NS,NR)) # SSP-Scenario x RCP scenario x RES scenario
 
+for r in range(0,NR): # RE scenario
+    Path = 'C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + FolderlistV_Sens[r] + '\\'
+    Resultfile = xlrd.open_workbook(Path + 'SysVar_TotalGHGFootprint.xls')
+    Resultsheet = Resultfile.sheet_by_name('TotalGHGFootprint')
+    for s in range(0,NS): # SSP scenario
+        for t in range(0,35): # time
+            CumEmsV_Sens[s,r] += Resultsheet.cell_value(t +2, 2*(s+1))
+        AnnEmsV2030_Sens[s,r]  = Resultsheet.cell_value(16  , 2*(s+1))
+        AnnEmsV2050_Sens[s,r]  = Resultsheet.cell_value(36  , 2*(s+1))
+        
+CumEmsB_Sens     = np.zeros((NS,NR)) # SSP-Scenario x RCP scenario x RES scenario
+AnnEmsB2030_Sens = np.zeros((NS,NR)) # SSP-Scenario x RCP scenario x RES scenario
+AnnEmsB2050_Sens = np.zeros((NS,NR)) # SSP-Scenario x RCP scenario x RES scenario
+
+for r in range(0,NR): # RE scenario
+    Path = 'C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + FolderlistB_Sens[r] + '\\'
+    Resultfile = xlrd.open_workbook(Path + 'SysVar_TotalGHGFootprint.xls')
+    Resultsheet = Resultfile.sheet_by_name('TotalGHGFootprint')
+    for s in range(0,NS): # SSP scenario
+        for t in range(0,35): # time
+            CumEmsB_Sens[s,r] += Resultsheet.cell_value(t +2, 2*(s+1))
+        AnnEmsB2030_Sens[s,r]  = Resultsheet.cell_value(16  , 2*(s+1))
+        AnnEmsB2050_Sens[s,r]  = Resultsheet.cell_value(36  , 2*(s+1))
+        
+     
+
+### Tornado plot for sensitivity
+        
+MyColorCycle = pylab.cm.Set1(np.arange(0,1,0.1)) # select 12 colors from the 'Paired' color map.            
+
+Title = ['Passenger vehicles','residential buildings']
+Scens = ['LED','SSP1','SSP2']
+LWE   = ['FabYieldImprovement','EoL_RR_Improvement','ChangeMaterialCompos.','ReduceMaterialContent','ReUse_Materials','LifeTimeExtension','MoreIntenseUse']
+
+#2030 emissions
+
+for m in range(0,NS): # SSP
+    for n in range(0,2): # Veh/Buildings
+        
+        if n == 0:
+            Data = AnnEmsV2030_Sens[:,1::]-np.einsum('S,n->Sn',AnnEmsV2030_Sens[:,0],np.ones(7))
+            Base = AnnEmsV2030_Sens[:,0]
+            
+        if n == 1:
+            Data = AnnEmsB2030_Sens[:,1::]-np.einsum('S,n->Sn',AnnEmsB2030_Sens[:,0],np.ones(7))
+            Base = AnnEmsB2030_Sens[:,0]
+            
+        # plot results
+    
+        fig  = plt.figure(figsize=(5,8))
+        ax1  = plt.axes([0.08,0.08,0.85,0.9])
+            
+        Poss = np.arange(7,0,-1)
+        ax1.barh(Poss,Data[m,:], color=MyColorCycle, lw=0.4)
+
+        # plot text and labels
+        for mm in range(0,7):
+            plt.text(Data[m,:].min()*0.9, 7.3-mm, LWE[mm],fontsize=14,fontweight='bold')          
+            plt.text(15, 7.3-mm, ("%3.0f" % Data[m,mm]),fontsize=14,fontweight='bold')          
+        
+        plt.text(Data[m,:].min()*0.55, 7.8, 'Baseline: ' + ("%3.0f" % Base[m]) + ' Mt/yr.',fontsize=14,fontweight='bold')
+
+        plt.title('2030 GHG emissions, sensitivity, ' + Region + '_ ' + Title[n] + '_' + Scens[m] + '.', fontsize = 18)
+        plt.ylabel('RE strategies.', fontsize = 18)
+        plt.xlabel('Mt of CO2-eq.', fontsize = 14)
+        #plt.xticks([0.25,1.25,2.25,3.25,4.25,5.25])
+        plt.yticks([])
+        #ax1.set_xticklabels([], rotation =90, fontsize = 21, fontweight = 'normal')
+        #plt_lgd  = plt.legend(handles = ProxyHandlesList,labels = LWE,shadow = False, prop={'size':16},ncol=1, loc = 'upper right' ,bbox_to_anchor=(1.91, 1)) 
+        plt.axis([Data[m,:].min() -15, Data[m,:].max() +80, 0.7, 8.1])
+    
+        plt.show()
+        fig_name = '2030_GHG_Sens_' + Region + '_ ' + Title[n] + '_' + Scens[m] + '.png'
+        fig.savefig('C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + fig_name, dpi = 400, bbox_inches='tight')             
+            
+        fig  = plt.figure(figsize=(5,8))
+        ax1  = plt.axes([0.08,0.08,0.85,0.9])
+            
+        Poss = np.arange(7,0,-1)
+        ax1.barh(Poss,Data[m,:], color=MyColorCycle, lw=0.4)
+
+        # plot text and labels
+        for mm in range(0,7):
+            plt.text(Data[m,:].min()*0.9, 7.3-mm, LWE[mm] + ': ' + ("%3.0f" % Data[m,mm]),fontsize=14,fontweight='bold')          
+        
+        plt.text(Data[m,:].min()*0.55, 7.8, 'Baseline: ' + ("%3.0f" % Base[m]) + ' Mt/yr.',fontsize=14,fontweight='bold')
+
+        plt.title('2030 GHG emissions, sensitivity, ' + Region + '_ ' + Title[n] + '_' + Scens[m] + '.', fontsize = 18)
+        plt.ylabel('RE strategies.', fontsize = 18)
+        plt.xlabel('Mt of CO2-eq.', fontsize = 14)
+        #plt.xticks([0.25,1.25,2.25,3.25,4.25,5.25])
+        plt.yticks([])
+        #ax1.set_xticklabels([], rotation =90, fontsize = 21, fontweight = 'normal')
+        #plt_lgd  = plt.legend(handles = ProxyHandlesList,labels = LWE,shadow = False, prop={'size':16},ncol=1, loc = 'upper right' ,bbox_to_anchor=(1.91, 1)) 
+        plt.axis([Data[m,:].min() -15, Data[m,:].max() +80, 0.7, 8.1])
+    
+        plt.show()
+        fig_name = '2030_GHG_Sens_' + Region + '_ ' + Title[n] + '_' + Scens[m] + '_V2.png'
+        fig.savefig('C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + fig_name, dpi = 400, bbox_inches='tight')             
+      
+        
+#2050 emissions
+
+for m in range(0,NS): # SSP
+    for n in range(0,2): # Veh/Buildings
+        
+        if n == 0:
+            Data = AnnEmsV2050_Sens[:,1::]-np.einsum('S,n->Sn',AnnEmsV2050_Sens[:,0],np.ones(7))
+            Base = AnnEmsV2050_Sens[:,0]
+            
+        if n == 1:
+            Data = AnnEmsB2050_Sens[:,1::]-np.einsum('S,n->Sn',AnnEmsB2050_Sens[:,0],np.ones(7))
+            Base = AnnEmsB2050_Sens[:,0]
+            
+        # plot results
+    
+        fig  = plt.figure(figsize=(5,8))
+        ax1  = plt.axes([0.08,0.08,0.85,0.9])
+            
+        Poss = np.arange(7,0,-1)
+        ax1.barh(Poss,Data[m,:], color=MyColorCycle, lw=0.4)
+
+        # plot text and labels
+        for mm in range(0,7):
+            plt.text(Data[m,:].min()*0.9, 7.3-mm, LWE[mm],fontsize=14,fontweight='bold')          
+            plt.text(15, 7.3-mm, ("%3.0f" % Data[m,mm]),fontsize=14,fontweight='bold')          
+        
+        plt.text(Data[m,:].min()*0.55, 7.8, 'Baseline: ' + ("%3.0f" % Base[m]) + ' Mt/yr.',fontsize=14,fontweight='bold')
+
+        plt.title('2050 GHG emissions, sensitivity, ' + Region + '_ ' + Title[n] + '_' + Scens[m] + '.', fontsize = 18)
+        plt.ylabel('RE strategies.', fontsize = 18)
+        plt.xlabel('Mt of CO2-eq.', fontsize = 14)
+        #plt.xticks([0.25,1.25,2.25,3.25,4.25,5.25])
+        plt.yticks([])
+        #ax1.set_xticklabels([], rotation =90, fontsize = 21, fontweight = 'normal')
+        #plt_lgd  = plt.legend(handles = ProxyHandlesList,labels = LWE,shadow = False, prop={'size':16},ncol=1, loc = 'upper right' ,bbox_to_anchor=(1.91, 1)) 
+        plt.axis([Data[m,:].min() -15, Data[m,:].max() +80, 0.7, 8.1])
+    
+        plt.show()
+        fig_name = '2050_GHG_Sens_' + Region + '_ ' + Title[n] + '_' + Scens[m] + '.png'
+        fig.savefig('C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + fig_name, dpi = 400, bbox_inches='tight')             
+            
+        fig  = plt.figure(figsize=(5,8))
+        ax1  = plt.axes([0.08,0.08,0.85,0.9])
+            
+        Poss = np.arange(7,0,-1)
+        ax1.barh(Poss,Data[m,:], color=MyColorCycle, lw=0.4)
+
+        # plot text and labels
+        for mm in range(0,7):
+            plt.text(Data[m,:].min()*0.9, 7.3-mm, LWE[mm] + ': ' + ("%3.0f" % Data[m,mm]),fontsize=14,fontweight='bold')          
+        
+        plt.text(Data[m,:].min()*0.55, 7.8, 'Baseline: ' + ("%3.0f" % Base[m]) + ' Mt/yr.',fontsize=14,fontweight='bold')
+
+        plt.title('2050 GHG emissions, sensitivity, ' + Region + '_ ' + Title[n] + '_' + Scens[m] + '.', fontsize = 18)
+        plt.ylabel('RE strategies.', fontsize = 18)
+        plt.xlabel('Mt of CO2-eq.', fontsize = 14)
+        #plt.xticks([0.25,1.25,2.25,3.25,4.25,5.25])
+        plt.yticks([])
+        #ax1.set_xticklabels([], rotation =90, fontsize = 21, fontweight = 'normal')
+        #plt_lgd  = plt.legend(handles = ProxyHandlesList,labels = LWE,shadow = False, prop={'size':16},ncol=1, loc = 'upper right' ,bbox_to_anchor=(1.91, 1)) 
+        plt.axis([Data[m,:].min() -15, Data[m,:].max() +80, 0.7, 8.1])
+    
+        plt.show()
+        fig_name = '2050_GHG_Sens_' + Region + '_ ' + Title[n] + '_' + Scens[m] + '_V2.png'
+        fig.savefig('C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + fig_name, dpi = 400, bbox_inches='tight')             
+      
+#2050 cum. emissions
+
+for m in range(0,NS): # SSP
+    for n in range(0,2): # Veh/Buildings
+        
+        if n == 0:
+            Data = CumEmsV_Sens[:,1::]-np.einsum('S,n->Sn',CumEmsV_Sens[:,0],np.ones(7))
+            Base = CumEmsV_Sens[:,0]
+        if n == 1:
+            Data = CumEmsB_Sens[:,1::]-np.einsum('S,n->Sn',CumEmsB_Sens[:,0],np.ones(7))
+            Base = CumEmsB_Sens[:,0]
+    
+        # plot results
+    
+        fig  = plt.figure(figsize=(5,8))
+        ax1  = plt.axes([0.08,0.08,0.85,0.9])
+            
+        Poss = np.arange(7,0,-1)
+
+        ax1.barh(Poss,Data[m,:], color=MyColorCycle, lw=0.4)
+        
+        # plot text and labels
+        for mm in range(0,7):
+            plt.text(Data[m,:].min() *0.9, 7.3-mm, LWE[mm],fontsize=14,fontweight='bold')          
+            plt.text(-Data[m,:].min() *0.1, 7.3-mm, ("%2.0f" % Data[m,mm]),fontsize=14,fontweight='bold')          
+
+        plt.text(Data[m,:].min()*0.7, 7.8, 'Baseline: ' + ("%2.0f" % Base[m]) + ' Mt/yr.',fontsize=14,fontweight='bold')
+        plt.title('2016-2050 cum. GHG, sensitivity, ' + Region + '_ ' + Title[n] + '_' + Scens[m] + '.', fontsize = 18)
+        plt.ylabel('RE strategies.', fontsize = 18)
+        plt.xlabel('Mt of CO2-eq.', fontsize = 14)
+        #plt.xticks([0.25,1.25,2.25,3.25,4.25,5.25])
+        plt.yticks([])
+        #ax1.set_xticklabels([], rotation =90, fontsize = 21, fontweight = 'normal')
+        #plt_lgd  = plt.legend(handles = ProxyHandlesList,labels = LWE,shadow = False, prop={'size':16},ncol=1, loc = 'upper right' ,bbox_to_anchor=(1.91, 1)) 
+        plt.axis([Data[m,:].min() *1.05, -Data[m,:].min() *0.05, 0.7, 8.1])
+    
+        plt.show()
+        fig_name = 'Cum_GHG_Sens_' + Region + '_ ' + Title[n] + '_' + Scens[m] + '.png'
+        fig.savefig('C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + fig_name, dpi = 400, bbox_inches='tight')             
+            
+
+### Area plot RE
+        
+        
+# Select scenario list: same as for bar chart above
+# E.g. for the USA, run code lines 41 to 59.
+
+NS = 3
+NC = 2
+NR = 5
+Nt = 35
+
+AnnEmsV = np.zeros((Nt,NS,NC,NR)) # SSP-Scenario x RCP scenario x RES scenario
+AnnEmsB = np.zeros((Nt,NS,NC,NR)) # SSP-Scenario x RCP scenario x RES scenario
+
+for r in range(0,NR): # RE scenario
+    Path = 'C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + FolderlistV[r] + '\\'
+    Resultfile = xlrd.open_workbook(Path + 'SysVar_TotalGHGFootprint.xls')
+    Resultsheet = Resultfile.sheet_by_name('TotalGHGFootprint')
+    for s in range(0,NS): # SSP scenario
+        for c in range(0,NC):
+            for t in range(0,35): # time
+                AnnEmsV[t,s,c,r] = Resultsheet.cell_value(t +2, 1 + c + NC*s)
+
+for r in range(0,NR): # RE scenario
+    Path = 'C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + FolderlistB[r] + '\\'
+    Resultfile = xlrd.open_workbook(Path + 'SysVar_TotalGHGFootprint.xls')
+    Resultsheet = Resultfile.sheet_by_name('TotalGHGFootprint')
+    for s in range(0,NS): # SSP scenario
+        for c in range(0,NC):
+            for t in range(0,35): # time
+                AnnEmsB[t,s,c,r] = Resultsheet.cell_value(t +2, 1 + c + NC*s)
+        
+# Area plot, stacked
+MyColorCycle = pylab.cm.Set1(np.arange(0,1,0.1)) # select 12 colors from the 'Paired' color map.            
+grey0_9      = np.array([0.9,0.9,0.9,1])
+
+Title      = ['Passenger vehicles','residential buildings']
+Scens      = ['LED','SSP1','SSP2']
+LWE_area   = ['recycling improvement','light-weighting','re-use','more intense use']     
+
+#mS = 1
+#mR = 1
+mRCP = 1 # select RCP2.6, which has full implementation of RE strategies by 2050.
+for mS in range(0,NS): # SSP
+    for mR in range(0,2): # Veh/Buildings
+        
+        if mR == 0:
+            Data = AnnEmsV[:,mS,mRCP,:]
+        if mR == 1:
+            Data = AnnEmsB[:,mS,mRCP,:]
+    
+    
+        fig  = plt.figure(figsize=(8,5))
+        ax1  = plt.axes([0.08,0.08,0.85,0.9])
+        
+        ProxyHandlesList = []   # For legend     
+        
+        # plot bars for domestic footprint
+        ax1.fill_between(np.arange(2016,2051),np.zeros((Nt)), Data[:,-1], linestyle = '-', facecolor = grey0_9, linewidth = 1.0)
+        ProxyHandlesList.append(plt.Rectangle((0, 0), 1, 1, fc=grey0_9)) # create proxy artist for legend
+        for m in range(4,0,-1):
+            ax1.fill_between(np.arange(2016,2051),Data[:,m], Data[:,m-1], linestyle = '-', facecolor = MyColorCycle[2*m,:], linewidth = 1.0)
+            ProxyHandlesList.append(plt.Rectangle((0, 0), 1, 1, fc=MyColorCycle[2*m,:])) # create proxy artist for legend
+            
+        #plt.text(Data[m,:].min()*0.55, 7.8, 'Baseline: ' + ("%3.0f" % Base[m]) + ' Mt/yr.',fontsize=14,fontweight='bold')
+        
+        plt.title('GHG emissions, stacked by RE strategy, \n' + Region + ', ' + Title[mR] + ', ' + Scens[mS] + '.', fontsize = 18)
+        plt.ylabel('Mt of CO2-eq.', fontsize = 18)
+        plt.xlabel('Year', fontsize = 18)
+        plt.xticks(fontsize=18)
+        plt.yticks(fontsize=18)
+        if mR == 0: # vehicles, legend lower left
+            plt_lgd  = plt.legend(handles = reversed(ProxyHandlesList),labels = LWE_area, shadow = False, prop={'size':16},ncol=1, loc = 'lower left')# ,bbox_to_anchor=(1.91, 1)) 
+        if mR == 1: # buildings, legend upper right
+            plt_lgd  = plt.legend(handles = reversed(ProxyHandlesList),labels = LWE_area, shadow = False, prop={'size':16},ncol=1, loc = 'upper right')# ,bbox_to_anchor=(1.91, 1)) 
+        ax1.set_xlim([2015, 2051])
+        
+        plt.show()
+        fig_name = 'GHG_TimeSeries_Stacked_' + Region + '_ ' + Title[mR] + '_' + Scens[mS] + '.png'
+        fig.savefig('C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\ODYM-RECC\\RECC_Results\\' + fig_name, dpi = 400, bbox_inches='tight')             
+           
 
 #
 #
