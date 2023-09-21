01c15a1a2d9b8888d07b420d67a778a2d1c2d9f5
= <=>
2023-09-21 16:42:12 +0200

    Automatic export formatter ODYM_RECC_Export_xlxs_Combine_Select_1.py now working, control file in result foldder, see email.

--- data_export/ODYM_RECC_Export_xlxs_Combine_Select_1.py
+++ data_export/ODYM_RECC_Export_xlxs_Combine_Select_1.py
@@ -19,7 +19,6 @@ import pandas as pd
 import uuid
 from pathlib import Path
 import RECC_Paths # Import path file
-pE = RECC_Paths.CRAFT_path
 
 def get_RECC_resfile_pos(Label,region,Resultsheet):
     # Find the index for the given Label
@@ -30,90 +29,160 @@ def get_RECC_resfile_pos(Label,region,Resultsheet):
         idx += 1 
     return idx
 
-# Aggregate region list (8)
-ARL = ['R5.2ASIA_Other',
-       'R5.2SSA_Other',
-       'R5.2LAM_Other',
-       'R5.2MNF_Other',
-       'R5.2REF_Other',
-       'R5.2OECD_Other',
-       'R32USACAN',
-       'Agg_EU+']
-
 # Create UUID of script run
 Current_UUID = str(uuid.uuid4())
-RECC_Paths.results_path_save = Path(RECC_Paths.results_path_eval) / ('RECC_Results_' + Current_UUID)
-if not os.path.exists(RECC_Paths.results_path_save): # Create scrip run results directory.
-    os.makedirs(RECC_Paths.results_path_save)
 
-# Definitions
-SpecPath      = os.path.join(pE,'CRAFT_Coupling_EXPORT_RECCv2.5.xlsx')
-SpecFile      = openpyxl.load_workbook(SpecPath)
-SpecSheet     = SpecFile['RECC_Export_CRAFT']
+# Read from default location:
+CP            = os.path.join(RECC_Paths.results_path,'RECCv2.5_EXPORT_Combine_Select_1.xlsx')    
+CF            = openpyxl.load_workbook(CP)
+CS            = CF['Cover'].cell(4,4).value
+    
+# Definitions/Specifications
+Model_id      = CF[CS].cell(1,2).value
+Model_date    = CF[CS].cell(1,4).value
+outpath       = CF[CS].cell(1,6).value
 
-ResFileP      = os.path.join(pE,'OUTPUTS - RECCv2.5_templateV4.xlsx')
-ResFile       = openpyxl.load_workbook(ResFileP)
-RS            = ResFile['data']
+
+# Prepare result workbook
+RB = openpyxl.Workbook() # Export other model results, calibration values, flags, etc.
+cs = RB.active
+cs.title = 'Cover'
+cs.cell(row=2, column=2).value = Model_id
+cs.cell(row=2, column=2).font = openpyxl.styles.Font(bold=True)
+cs.cell(row=4, column=2).value = Model_date
+rs = RB.create_sheet('Results')
 
 # Read specs and variable matchings
-scen = [] # list of scenarios
-rebf = [] # list of matching reb results folders
-nrbf = [] # list of matching nrb results folders
+scen = [] # list of target scenarios
 offs = [] # list of offsets for the different scenarios rel. to starting position
+secs = [[],[],[],[],[],[],[],[],[],[]] # list of matching results folders for sectors 1, 2, and 3 - 10
+secS = ['sector1','sector2','sector3','sector4','sector5','sector6','sector7','sector8','sector9','sector10']
 
-IndRW_Total = np.zeros([21,45])
+secL = [] # Sector labels
+for i in range(0,10):
+    secL.append(CF[CS].cell(3,6+i).value)
 
-for m in range(0,8):
-    scen.append(SpecSheet.cell(m+3,2).value)
-    rebf.append(SpecSheet.cell(m+3,6).value)
-    nrbf.append(SpecSheet.cell(m+3,7).value)
-    offs.append(SpecSheet.cell(m+3,5).value)
-    
-# iterate over scenarios, parse and write results:
-outline = 2    
-for m in range(14,1000):
-    if SpecSheet.cell(m,2).value is None:
+# Read scenarios
+r = 4
+while True:
+    if CF[CS].cell(r,1).value is None:
         break
-    Edits_pos = SpecSheet.cell(m,1).value
-    Edits_var = SpecSheet.cell(m,2).value
-    Edits_uni = SpecSheet.cell(m,3).value
-    Edits_reg = SpecSheet.cell(m,4).value
-    RECC_var  = SpecSheet.cell(m,6).value
-    RECC_reg  = SpecSheet.cell(m,8).value
-    Sector    = SpecSheet.cell(m,5).value
-    Conv_Fact = SpecSheet.cell(m,9).value
-    # check the four different scenarios:
-    if Sector == 'reb':
-        RECC_FList = rebf
-    if Sector == 'nrb':
-        RECC_FList = nrbf        
-    for r in range(0,8): # loop over the four scenarios
-        RECC_ResFile = [filename for filename in os.listdir(os.path.join(RECC_Paths.results_path,RECC_FList[r])) if filename.startswith('ODYM_RECC_ModelResults_')]
-        RECC_Resultfile2  = openpyxl.load_workbook(os.path.join(RECC_Paths.results_path,RECC_FList[r],RECC_ResFile[0]))
-        RECC_Resultsheet2 = RECC_Resultfile2['Model_Results']
-        idx = get_RECC_resfile_pos(RECC_var,RECC_reg,RECC_Resultsheet2)
-        # Write results to export file
-        RS.cell(outline,1).value = 'RECC v2.5'
-        RS.cell(outline,2).value = scen[r]
-        RS.cell(outline,3).value = RECC_Resultsheet2.cell(idx+offs[r],3).value # Region
-        RS.cell(outline,4).value = Edits_var # Variable
-        RS.cell(outline,5).value = Edits_uni # Unit
-        # write values
-        print(idx+offs[r])
-        for t in range(0,45):
-            RS.cell(outline,t+6).value = RECC_Resultsheet2.cell(idx+offs[r],t+9).value * Conv_Fact # Value to Excel
-            if Edits_pos is not None:
-                IndRW_Total[Edits_pos,t] += RECC_Resultsheet2.cell(idx+offs[r],t+9).value * Conv_Fact # Value to array that sums up reb and nrb
-        outline +=1
+    if CF[CS].cell(r,1).value == 1: # Only if the SELECT flag in col. A is set to 1.
+        scen.append(CF[CS].cell(r,2).value)
+        offs.append(CF[CS].cell(r,5).value)
+        for sl in range(0,10):
+            secs[0].append(CF[CS].cell(r,6+sl).value)
+    r += 1
+    
+# Move to parameter list:
+while True:
+    if CF[CS].cell(r,2).value == 'Target indicator label':
+        break    
+    r += 1
+r += 1    
 
-# Write array that sums up reb and nrb
-IndRW_Total[-1,:] = IndRW_Total[0:-1,:].sum(axis=0) # Global results as sum of individual countries
-for r in range(0,21):
-    for t in range(0,45):
-        RS.cell(outline+r,t+6).value = IndRW_Total[r,t] # Value to Excel
+# Read indicator list:
+ti = []    # target indicator
+ri = []    # RECC indicator
+tu = []    # target unit
+ru = []    # RECC unit
+cf = []    # Conv. factor
+sl = []    # sector list
+tr = []    # target region
+rr = []    # RECC regions
+
+r0 = r
+
+while True:
+    if CF[CS].cell(r,2).value is None:
+        break
+    ti.append(CF[CS].cell(r,2).value)
+    ri.append(CF[CS].cell(r,3).value)
+    tu.append(CF[CS].cell(r,4).value)
+    ru.append(CF[CS].cell(r,5).value)
+    cf.append(CF[CS].cell(r,6).value)
+    sl.append(CF[CS].cell(r,7).value)
+    tr.append(CF[CS].cell(r,8).value)
+    c = 9
+    rr.append([])
+    while True:
+        if CF[CS].cell(r,c).value is None:
+            break
+        rr[r-r0].append(CF[CS].cell(r,c).value)
+        c += 1
+    r += 1
+
+nos = len(scen) # number of scenarios
+noi = len(ti)   # number of indicators
+
+# Create sector list with actual labels, not placeholders
+sL = sl.copy()
+for i in range(0,7): # Find all sectorX instances and replace them with their actual sector labels
+    for j in range(0,10):
+        if secS[j] in sL[i]:
+            sL[i] = sL[i].replace(secS[j],secL[j])
+
+# iterate over scenarios, parse results:
+Res = np.zeros((nos*noi,46+7)) # main result array 46 years, one blank column, and 6 columns for cumulative results
+
+for S in range(0,nos): # iterate over all selected scenarios
+    for s in range(0,3): # iterate over all sectors 
+        rf = secs[s][S]# result folder
+        if rf is not None: # if a folder is given, extract all indicators and write to corresponding position in result array:
+            print('Reading data from ' + rf)
+            RECC_ResFile = [filename for filename in os.listdir(os.path.join(RECC_Paths.results_path,rf)) if filename.startswith('ODYM_RECC_ModelResults_')]
+            RECC_RF      = openpyxl.load_workbook(os.path.join(RECC_Paths.results_path,rf,RECC_ResFile[0]))
+            RECC_RS      = RECC_RF['Model_Results']
+            for ind in range(0,noi): # for all indicators
+                print('Reading data for ' + ti[ind])
+                for reg in range(0,len(rr[ind])): # for all regions to be aggreated
+                    idx = get_RECC_resfile_pos(ri[ind],rr[ind][reg],RECC_RS) # position of that indicator for that region in the RECC result file
+                    if secS[s] in sl[ind]: # if the current sector is in the sector list for this indicator
+                        targetpos = ind * nos + S # position in Res array: outer index: indicator, inner index: scenario
+                        for t in range(0,46): # read and add values
+                            Res[targetpos,t] += RECC_RS.cell(idx+offs[S],t+8).value * cf[ind] # Value from Excel to array
+
+# Determine cumulative quantities:
+Res[:,47] = Res[:,5:36].sum(axis=1) # Cum. 2020-2050
+Res[:,48] = Res[:,5:46].sum(axis=1) # Cum. 2020-2060
+Res[:,49] = Res[:,5:16].sum(axis=1) # Cum. 2020-2030
+Res[:,50] = Res[:,15:26].sum(axis=1) # Cum. 2030-2040
+Res[:,51] = Res[:,25:36].sum(axis=1) # Cum. 2040-2050
+Res[:,52] = Res[:,35:46].sum(axis=1) # Cum. 2050-2060
+
+# Export results to xlsx
+# Define column labels
+rs.cell(row=1, column=1).value = 'Model id'
+rs.cell(row=1, column=2).value = 'Variable'
+rs.cell(row=1, column=3).value = 'Scenario'
+rs.cell(row=1, column=4).value = 'Region'
+rs.cell(row=1, column=5).value = 'Sectors'
+rs.cell(row=1, column=6).value = 'Unit'
+for t in range(0,46):
+    rs.cell(row=1, column=7+t).value = 2015 + t
+rs.cell(row=1, column=47+7).value = 'Cum. 2020-2050'
+rs.cell(row=1, column=48+7).value = 'Cum. 2020-2060'
+rs.cell(row=1, column=49+7).value = 'Cum. 2020-2030'
+rs.cell(row=1, column=50+7).value = 'Cum. 2030-2040'
+rs.cell(row=1, column=51+7).value = 'Cum. 2040-2050'
+rs.cell(row=1, column=52+7).value = 'Cum. 2050-2060'
+for t in range(1,60):
+    rs.cell(row=1, column=t).font = openpyxl.styles.Font(bold=True)
+# fill labels
+for m in range(0,nos*noi):
+     rs.cell(row=m+2, column=1).value = Model_id 
+     rs.cell(row=m+2, column=2).value = ti[m // nos]
+     rs.cell(row=m+2, column=3).value = scen[m % nos]
+     rs.cell(row=m+2, column=4).value = tr[m // nos]
+     rs.cell(row=m+2, column=5).value = sL[m // nos]
+     rs.cell(row=m+2, column=6).value = tu[m // nos]
+# fill data    
+for m in range(0,nos*noi):
+    for n in range(0,46+7):
+        rs.cell(row=2+m, column=7+n).value = Res[m,n]
 
 # Save exported results
-ResFile.save(os.path.join(pE,'OUTPUTS - RECCv2.5_CRAFT_Coupling.xlsx')) 
+RB.save(os.path.join(RECC_Paths.export_path,outpath,'Results_RECCv2.5_CRAFT_Coupling.xlsx')) 
                  
 #
 #
